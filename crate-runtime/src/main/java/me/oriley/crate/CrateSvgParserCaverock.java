dd955ea3a9f085efc1a371cca1c195333c810618
Kane O'Riley <kane@oriley.me>
2016-03-20 02:19:23 +1030

    Use public API for parsing SVG to PictureDrawable
    Not sure how I missed that previously

--- crate-runtime/src/main/java/me/oriley/crate/CrateSvgParserCaverock.java
+++ crate-runtime/src/main/java/me/oriley/crate/CrateSvgParserCaverock.java
@@ -20,7 +20,6 @@ import android.graphics.drawable.PictureDrawable;
 import android.support.annotation.Nullable;
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGParseException;
-import com.caverock.androidsvg.SVGParser;
 import me.oriley.crate.CrateSvg.SvgParseException;
 
 import java.io.IOException;
@@ -39,7 +38,7 @@ public final class CrateSvgParserCaverock implements CrateSvg.Parser {
         try {
             //noinspection TryFinallyCanBeTryWithResources
             try {
-                SVG svg = new ExposedParser().parse(stream);
+                SVG svg = SVG.getFromInputStream(stream);
                 if (svg != null) {
                     drawable = new PictureDrawable(svg.renderToPicture());
                 }
@@ -49,14 +48,7 @@ public final class CrateSvgParserCaverock implements CrateSvg.Parser {
         } catch (IOException | SVGParseException e) {
             throw new SvgParseException(e.getCause());
         }
-        return drawable;
-    }
 
-    // Subclassing to expose parse method
-    private static final class ExposedParser extends SVGParser {
-        @Override
-        public SVG parse(InputStream is) throws SVGParseException {
-            return super.parse(is);
-        }
+        return drawable;
     }
 }
